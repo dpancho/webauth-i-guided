8a7c9d2f0ef4737b0de51185c19285e44a4b0e22
Dale Pancho <dalepancho7@gmail.com>
2019-11-11 09:36:26 -0800

    I've been trying to catch up to what he changed 30 min before class

--- api/api-router.js
+++ api/api-router.js
@@ -10,4 +10,15 @@ router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
 
+router.post('/hash', (req,res) => {
+  const credentials = req.body;
+
+  const hash = bcrypt.hashSync(credentials.password, 14);
+
+  console.log( `password: ${credentials}` )
+  console.log( `hash: ${hash}` )
+
+  // credentials.password = hash;
+})
+
 module.exports = router;
